9aa87ebe0c465a0f80750cdfbd91b5aca1a818f3
Nicholas Cirone <ncirone@live.unc.edu>
2022-10-14 16:55:16 -0400

    prettify all js files

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,4 +1,9 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
 const db = require("../mongoDB");
 module.exports = {
   name: "nowplaying",
@@ -6,41 +11,53 @@ module.exports = {
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
-    let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
-    lang = lang?.language || client.language
+    let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id });
+    lang = lang?.language || client.language;
     lang = require(`../languages/${lang}.js`);
 
     try {
-
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing)
+        return interaction
+          .reply({ content: lang.msg5, ephemeral: true })
+          .catch((e) => {});
 
       const track = queue.songs[0];
-      if (!track) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+      if (!track)
+        return interaction
+          .reply({ content: lang.msg5, ephemeral: true })
+          .catch((e) => {});
 
       const embed = new EmbedBuilder();
       embed.setColor(client.config.embedColor);
       embed.setThumbnail(track.thumbnail);
-      embed.setTitle(track.name)
+      embed.setTitle(track.name);
       embed.setDescription(`> Audio \`%${queue.volume}\`
 > Duration \`${track.formattedDuration}\`
 > URL: **${track.url}**
-> Loop Mode \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'}\`
-> Filter: \`${queue.filters.names.join(', ') || 'Off'}\`
+> Loop Mode \`${
+        queue.repeatMode
+          ? queue.repeatMode === 2
+            ? "All Queue"
+            : "This Song"
+          : "Off"
+      }\`
+> Filter: \`${queue.filters.names.join(", ") || "Off"}\`
 > By: <@${track.user.id}>`);
 
       embed.setTimestamp();
-      embed.setFooter({ text: `codeshare.me | Umut Bayraktar ❤️` })
+      embed.setFooter({ text: `codeshare.me | Umut Bayraktar ❤️` });
 
       const saveButton = new ButtonBuilder();
       saveButton.setLabel(lang.msg47);
-      saveButton.setCustomId('saveTrack');
+      saveButton.setCustomId("saveTrack");
       saveButton.setStyle(ButtonStyle.Success);
 
       const row = new ActionRowBuilder().addComponents(saveButton);
 
-      interaction.reply({ embeds: [embed], components: [row] }).catch(e => { })
-
+      interaction
+        .reply({ embeds: [embed], components: [row] })
+        .catch((e) => {});
     } catch (e) {
       if (client.errorLog) {
         let embed = new EmbedBuilder()
@@ -49,13 +66,33 @@ module.exports = {
           .addFields([
             { name: "Command", value: `${interaction?.commandName}` },
             { name: "Error", value: `${e.stack}` },
-            { name: "User", value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``, inline: true },
-            { name: "Guild", value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``, inline: true },
-            { name: "Time", value: `<t:${Math.floor(Date.now() / 1000)}:R>`, inline: true },
-            { name: "Command Usage Channel", value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``, inline: true },
-            { name: "User Voice Channel", value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``, inline: true },
-          ])
-        await client.errorLog.send({ embeds: [embed] }).catch(e => { })
+            {
+              name: "User",
+              value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``,
+              inline: true,
+            },
+            {
+              name: "Guild",
+              value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``,
+              inline: true,
+            },
+            {
+              name: "Time",
+              value: `<t:${Math.floor(Date.now() / 1000)}:R>`,
+              inline: true,
+            },
+            {
+              name: "Command Usage Channel",
+              value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``,
+              inline: true,
+            },
+            {
+              name: "User Voice Channel",
+              value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``,
+              inline: true,
+            },
+          ]);
+        await client.errorLog.send({ embeds: [embed] }).catch((e) => {});
       } else {
         console.log(`
     Command: ${interaction?.commandName}
@@ -64,9 +101,11 @@ module.exports = {
     Guild: ${interaction?.guild?.name} (${interaction?.guild?.id})
     Command Usage Channel: ${interaction?.channel?.name} (${interaction?.channel?.id})
     User Voice Channel: ${interaction?.member?.voice?.channel?.name} (${interaction?.member?.voice?.channel?.id})
-    `)
+    `);
       }
-      return interaction.reply({ content: `${lang.error7}\n\`${e}\``, ephemeral: true }).catch(e => { })
+      return interaction
+        .reply({ content: `${lang.error7}\n\`${e}\``, ephemeral: true })
+        .catch((e) => {});
     }
   },
 };
--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder } = require('discord.js')
+const { EmbedBuilder } = require("discord.js");
 const db = require("../mongoDB");
 module.exports = {
   name: "ping",
@@ -6,29 +6,33 @@ module.exports = {
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
-    let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
-    lang = lang?.language || client.language
+    let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id });
+    lang = lang?.language || client.language;
     lang = require(`../languages/${lang}.js`);
 
     try {
-
       const start = Date.now();
-      interaction.reply("Pong!").then(msg => {
-        const end = Date.now();
-        const embed = new EmbedBuilder()
-          .setColor(client.config.embedColor)
-          .setTitle(client.user.username + " - Pong!")
-          .setThumbnail(client.user.displayAvatarURL())
-          .addFields([
-            { name: lang.msg49, value: `\`${end - start}ms\` 🛰️` },
-            { name: lang.msg50, value: `\`${Date.now() - start}ms\` 🛰️` },
-            { name: lang.msg51, value: `\`${Math.round(client.ws.ping)}ms\` 🛰️` }
-          ])
-          .setTimestamp()
-          .setFooter({ text: `codeshare.me | Umut Bayraktar ❤️` })
-        return interaction.editReply({ embeds: [embed] }).catch(e => { });
-      }).catch(err => { })
-
+      interaction
+        .reply("Pong!")
+        .then((msg) => {
+          const end = Date.now();
+          const embed = new EmbedBuilder()
+            .setColor(client.config.embedColor)
+            .setTitle(client.user.username + " - Pong!")
+            .setThumbnail(client.user.displayAvatarURL())
+            .addFields([
+              { name: lang.msg49, value: `\`${end - start}ms\` 🛰️` },
+              { name: lang.msg50, value: `\`${Date.now() - start}ms\` 🛰️` },
+              {
+                name: lang.msg51,
+                value: `\`${Math.round(client.ws.ping)}ms\` 🛰️`,
+              },
+            ])
+            .setTimestamp()
+            .setFooter({ text: `codeshare.me | Umut Bayraktar ❤️` });
+          return interaction.editReply({ embeds: [embed] }).catch((e) => {});
+        })
+        .catch((err) => {});
     } catch (e) {
       if (client.errorLog) {
         let embed = new EmbedBuilder()
@@ -37,13 +41,33 @@ module.exports = {
           .addFields([
             { name: "Command", value: `${interaction?.commandName}` },
             { name: "Error", value: `${e.stack}` },
-            { name: "User", value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``, inline: true },
-            { name: "Guild", value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``, inline: true },
-            { name: "Time", value: `<t:${Math.floor(Date.now() / 1000)}:R>`, inline: true },
-            { name: "Command Usage Channel", value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``, inline: true },
-            { name: "User Voice Channel", value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``, inline: true },
-          ])
-        await client.errorLog.send({ embeds: [embed] }).catch(e => { })
+            {
+              name: "User",
+              value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``,
+              inline: true,
+            },
+            {
+              name: "Guild",
+              value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``,
+              inline: true,
+            },
+            {
+              name: "Time",
+              value: `<t:${Math.floor(Date.now() / 1000)}:R>`,
+              inline: true,
+            },
+            {
+              name: "Command Usage Channel",
+              value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``,
+              inline: true,
+            },
+            {
+              name: "User Voice Channel",
+              value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``,
+              inline: true,
+            },
+          ]);
+        await client.errorLog.send({ embeds: [embed] }).catch((e) => {});
       } else {
         console.log(`
     Command: ${interaction?.commandName}
@@ -52,9 +76,11 @@ module.exports = {
     Guild: ${interaction?.guild?.name} (${interaction?.guild?.id})
     Command Usage Channel: ${interaction?.channel?.name} (${interaction?.channel?.id})
     User Voice Channel: ${interaction?.member?.voice?.channel?.name} (${interaction?.member?.voice?.channel?.id})
-    `)
+    `);
       }
-      return interaction.reply({ content: `${lang.error7}\n\`${e}\``, ephemeral: true }).catch(e => { })
+      return interaction
+        .reply({ content: `${lang.error7}\n\`${e}\``, ephemeral: true })
+        .catch((e) => {});
     }
   },
 };
